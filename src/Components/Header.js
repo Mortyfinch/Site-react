16854bad25339555e1c951b2362e47c760ff54fd
Mortyfinch <62078244+Mortyfinch@users.noreply.github.com>
2020-05-21 21:36:46 +0300

    init

---
+++ src/Components/Header.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import {
+  Navbar,
+  Nav,
+  FormControl,
+  Container,
+  Form,
+  Button,
+} from "react-bootstrap";
+import logo from "./logo192.png";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from '../Pages/Home';
+import About from '../Pages/Blog';
+import Contacts from '../Pages/Contacts';
+import Blog from '../Pages/Blog';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <>
+        <Navbar
+          fixed="top"
+          collapse0nSelect
+          expand="md"
+          bg="dark"
+          variant="dark"
+        >
+          <Container>
+            <Navbar.Brand href="/">
+              <img
+                src={logo}
+                height="30"
+                width="30"
+                className="d-inline-block align-top"
+                alt="logo"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <nav className="mr-auto">
+                <Nav.Link href="/"> Home </Nav.Link>
+                <Nav.Link href="/about"> About </Nav.Link>
+                <Nav.Link href="/contacts"> Contacts </Nav.Link>
+                <Nav.Link href="/blog"> Blog </Nav.Link>
+              </nav>
+              <form inline>
+                <FormControl
+                  type="text"
+                  placeholder="Search"
+                  className="mr-sm-2"
+                />
+                <button variant="outline-info">Search</button>
+              </form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contacts" component={Contacts} />
+            <Route exact path="/blog" component={Blog} />
+          </Switch>
+        </Router>
+      </>
+    );
+  }
+}
